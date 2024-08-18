cb81a52f888dc5dff8d2ce32e52e697236669132
Vinoth-94 <144323939+Vinoth-94@users.noreply.github.com>
2024-08-18 18:25:02 +0530

    updated project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
-import easybank from "./assets/easybank.jpg"
-import expense from "./assets/expense.png"
-import todo from "./assets/todo.jpg"
+import easybank from "./assets/easybank.jpg";
+import expense from "./assets/expense.png";
+import todo from "./assets/todo.jpg";
+import cart from "./assets/Cart.jpg";
 
 export default function Projects() {
   return (
@@ -14,13 +15,38 @@ export default function Projects() {
               className="card col-md-3 col-12 mx-2 mb-1"
               style={{ width: "14rem" }}
             >
+              <img className="card-img-top" src={cart} alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title">Simple Cart </h5>
+                <a
+                  href="https://github.com/Vinoth-94/sam_cart"
+                  target="_blank"
+                  className="btn btn-primary"
+                >
+                  Github
+                </a>
+                <a
+                  href="https://vinoth-94.github.io/sam_cart/"
+                  target="_blank"
+                  className="btn btn-secondary"
+                >
+                  Demo
+                </a>
+              </div>
+            </div>
+            <div
+              className="card col-md-3 col-12 mx-2 mb-1"
+              style={{ width: "14rem" }}
+            >
               <img
                 className="card-img-top"
                 src={easybank}
                 alt="Card image cap"
               />
-            <div className="card-body">
-                <h5 className="card-title">Easy bank Responsive design using media query </h5>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Easy bank Responsive design using media query{" "}
+                </h5>
                 <a
                   href="https://github.com/Vinoth-94/easybank-project"
                   target="_blank"
@@ -46,7 +72,7 @@ export default function Projects() {
                 src={expense}
                 alt="Card image cap"
               />
-             <div className="card-body">
+              <div className="card-body">
                 <h5 className="card-title">Expense Tracker </h5>
                 <a
                   href="https://github.com/Vinoth-94/expense_tracker"
@@ -60,7 +86,7 @@ export default function Projects() {
                   target="_blank"
                   className="btn btn-secondary"
                 >
-               Demo
+                  Demo
                 </a>
               </div>
             </div>
@@ -68,12 +94,8 @@ export default function Projects() {
               className="card col-md-3 col-12 mx-2 mb-1"
               style={{ width: "14rem" }}
             >
-              <img
-                className="card-img-top"
-                src={todo}
-                alt="Card image cap"
-              />
-             <div className="card-body">
+              <img className="card-img-top" src={todo} alt="Card image cap" />
+              <div className="card-body">
                 <h5 className="card-title">Todo list</h5>
                 <a
                   href="https://github.com/Vinoth-94/todo-list"
